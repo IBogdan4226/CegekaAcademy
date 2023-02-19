8d87017f1d30614e581ec4180c85228675dad3b7
IBogdan4226 <bogdan-marian.ionita@student.tuiasi.ro>
2023-02-19 20:16:32 +0200

    Tema 2 completa+bonus

---
+++ Tema 02 - SQL & ORM/PetShelterDB+CLI/PetShelter.DataAccessLayer/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace PetShelter.DataAccessLayer.Models;
+
+public class Person: IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    /// <summary>
+    ///     Can be used to check if the person is adult or not
+    /// </summary>
+    public DateTime? DateOfBirth { get; set; }
+
+    public string IdNumber { get; set; }
+
+    public ICollection<Pet> RescuedPets { get; set; }
+    public ICollection<Pet> AdoptedPets { get; set; }
+    public ICollection<Donation> Donations { get; set; }
+
+    public Person( string name,  string idNumber)
+    {
+        Name = name;
+        this.DateOfBirth= DateTime.Now;
+        IdNumber = idNumber;
+    }
+}
---
+++ Tema 02 - SQL & ORM/PetShelterDB+CLI/PetShelter.DataAccessLayer/Models/Pet.cs
@@ -0,0 +1,58 @@
+﻿namespace PetShelter.DataAccessLayer.Models;
+
+public class Pet: IEntity
+{
+    /// <summary>
+    ///     Identifier
+    /// </summary>
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    /// <summary>
+    ///     e.g. Cat, Dog etc.
+    /// </summary>
+    public string Type { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime Birthdate { get; set; }
+    
+    public bool IsHealthy { get; set; }
+    
+    public decimal WeightInKg { get; set; }
+
+    /// <summary>
+    ///     Can be used for soft delete. The pet can be marked as not sheltered anymore if it was adopted or it died.
+    /// </summary>
+    public bool IsSheltered { get; set; }
+
+    /// <summary>
+    ///     FK to a person
+    /// </summary>
+    public int? RescuerId { get; set; }
+
+    /// <summary>
+    ///     FK to a person
+    /// </summary>
+    public int? AdopterId { get; set; }
+
+    public Person? Adopter { get; set; }
+    public Person Rescuer { get; set; }
+
+
+    public Pet(string name, string description)
+    {
+        ///For simplicity in cli
+        this.Name = name;
+        this.Description = description;
+        this.Birthdate = DateTime.Now;
+        this.IsHealthy = true;
+        this.ImageUrl = "";
+        this.IsSheltered=true;
+        this.WeightInKg = 2;
+        this.Type = "Pet";
+    }
+}
