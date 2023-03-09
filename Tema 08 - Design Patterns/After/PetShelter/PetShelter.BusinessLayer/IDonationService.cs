d41b8ba77285b4ecc50eb63bd2d7dacf3fabf2c5
IBogdan4226 <bogdan-marian.ionita@student.tuiasi.ro>
2023-03-09 18:55:01 +0200

    starting tema 8

---
+++ Tema 08 - Design Patterns/After/PetShelter/PetShelter.BusinessLayer/IDonationService.cs
@@ -0,0 +1,13 @@
+﻿using PetShelter.DataAccessLayer.Models;
+
+namespace PetShelter.BusinessLayer.Tests
+{
+    public interface IDonationService
+    {
+        Task<IReadOnlyCollection<Donation>> GetAllDonations();
+        Task AddDonation(AddDonationRequest addDonationRequest);
+        Task<Donation> GetDonation(int id);
+        Task UpdateDonation(int donationId, UpdateDonationRequest updateDonationRequest);
+    
+    }
+}
---
+++ Tema 08 - Design Patterns/After/PetShelter/PetShelter.BusinessLayer/Models/AddDonationRequest.cs
@@ -0,0 +1,9 @@
+﻿using PetShelter.BusinessLayer.Models;
+namespace PetShelter.BusinessLayer.Tests;
+
+public class AddDonationRequest
+{
+    public decimal Amount { get; set; }
+    public int DonorId { get; set; }
+    public Person Donor { get; set; }
+}
